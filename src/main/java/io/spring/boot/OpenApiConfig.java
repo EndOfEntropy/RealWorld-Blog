a3c47a3e1b43d6bb1f91cada15c756480aff8e65
EndOfEntropy <simonphoenix60@gmail.com>
2025-12-18 16:45:19 -0500

    Update project

--- src/main/java/io/spring/boot/OpenApiConfig.java
+++ src/main/java/io/spring/boot/OpenApiConfig.java
@@ -2,8 +2,11 @@ package io.spring.boot;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -11,6 +14,10 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+            .servers(List.of(
+                new Server().url("https://realworld-blog-production.up.railway.app").description("Production"),
+                new Server().url("http://localhost:3000").description("Local")
+            ))
             .info(new Info()
                 .title("RealWorld Blog API")
                 .version("1.0")
